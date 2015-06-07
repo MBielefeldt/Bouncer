7a5a3df89d0bbd4d4c8aa486c5d137aee99ea885
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-06-07 10:08:50 +0200

    Initial commit

---
+++ Bouncer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bouncer
+//
+//  Created by Mads Bielefeldt on 07/06/15.
+//  Copyright (c) 2015 GN ReSound. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
